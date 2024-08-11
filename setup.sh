325db2528ae071a73c29c174058df0a59f456a63
Vinay Venkatesh <>
2024-08-11 20:30:30 +0000

    Check for required tools

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+type entr >/dev/null 2>&1 || { echo >&2 "I require entr but it's not installed.  Aborting."; exit 1; }
+type fzf >/dev/null 2>&1 || { echo >&2 "I require fzf but it's not installed.  Aborting."; exit 1; }
+type bat >/dev/null 2>&1 || { echo >&2 "I require bat but it's not installed.  Aborting."; exit 1; }
+
 ln -sf "$HOME/.local/dotfiles/.bashrc" "$HOME/.bashrc"
 ln -sf "$HOME/.local/dotfiles/.bash_profile" "$HOME/.bash_profile"
 ln -sf "$HOME/.local/dotfiles/.gtdrc" "$HOME/.gtdrc"
